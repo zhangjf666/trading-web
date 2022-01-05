63b2e735e746b4c7a2e97dfd125d5f51b59f31a3
zjf <im.zhangjf@gmail.com>
2022-01-05 17:12:26 +0800

    北向资金回测策略页面

--- src/views/strategy/n2s/n2s-sign-test.vue
+++ src/views/strategy/n2s/n2s-sign-test.vue
@@ -3,9 +3,13 @@
     <!--搜索栏-->
     <div class="head-container">
       <el-form :inline="true" :model="query">
-        <el-form-item class="form-item" label="测试代码">
-            <el-input v-model="query.code"></el-input>
-        </el-form-item>  
+        <el-form-item class="form-item" label="回测代码">
+            <el-input v-model="query.code" style="width: 100px"></el-input>
+        </el-form-item>
+        <el-form-item class="form-item" label="代码类型">
+          <el-radio v-model="query.codeType" label="0">个股</el-radio>
+          <el-radio v-model="query.codeType" label="1">指数</el-radio>
+        </el-form-item>
         <el-form-item class="form-item" label="标准差取值">
             <el-input-number v-model="query.multiple" :step="0.5" step-strictly :min="1"></el-input-number>
         </el-form-item>
@@ -16,27 +20,27 @@
             class="form-item"
             label="回测时间"
           >
-            <el-input
-              v-model="query.startDate"
-              clearable
-              size="small"
-              placeholder="开始时间"
-              style="width: 130px"
-              class="filter-item"
-            />-
-            <el-input
-              v-model="query.endDate"
-              clearable
-              size="small"
-              placeholder="结束时间"
-              style="width: 130px"
-              class="filter-item"
-            />
+            <el-date-picker
+              ref="picker"
+              v-model="query.date"
+              type="daterange"
+              start-placeholder="开始日期"
+              end-placeholder="结束日期"
+              :default-time="['00:00:00', '23:59:59']"
+              :picker-options="pickerOptions"
+              value-format="yyyy-MM-dd"
+              >
+            </el-date-picker>
           </el-form-item>
         <el-form-item class="form-item" size="small">
           <el-button type="primary" @click="queryData">回测</el-button>
         </el-form-item>
       </el-form>
+      <div class="summary" v-if="Object.keys(summary).length != 0">
+        <p>代码: {{summary.code}}, 名称: {{summary.name}}</p>
+        <p>目标总收益率: {{summary.er}}%, 目标年收益率: {{summary.arr}}%, 最大回撤: {{summary.max_drawdown}}%, 最大回撤开始时间: {{summary.max_point}}, 最大回撤结束时间: {{summary.min_point}}</p>
+        <p>策略总收益率: {{summary.s_er}}%, 策略年收益率: {{summary.s_arr}}%, 最大回撤: {{summary.s_max_drawdown}}%, 最大回撤开始时间: {{summary.s_max_point}}, 最大回撤结束时间: {{summary.s_min_point}}</p>
+      </div>
     </div>
     <div class="chart-container">
         <line-chart id="n2s-sign" height="100%" width="100%" :option="option"></line-chart>
@@ -48,6 +52,7 @@
 import echarts from 'echarts'
 import LineChart from "@/components/Charts/LineChart"
 import { getN2sSignTest } from "@/api/trading";
+import util from "@/utils/my-utils"
 
 export default {
   name: "n2s-sign-test",
@@ -55,18 +60,46 @@ export default {
   data() {
     return {
       data: [],
+      summary: {},
       query: {
         code: '',
-        startDate: '',
-        endDate: '',
+        codeType: '0',
+        date: '',
         multiple: 2,
         period: 252
       },
+      pickerOptions: {
+          shortcuts: [{
+            text: '最近一年',
+            onClick(picker) {
+              const end = new Date();
+              const start = new Date();
+              start.setTime(start.getTime() - 3600 * 1000 * 24 * 365);
+              picker.$emit('pick', [start, end]);
+            }
+          }, {
+            text: '最近两年',
+            onClick(picker) {
+              const end = new Date();
+              const start = new Date();
+              start.setTime(start.getTime() - 3600 * 1000 * 24 * 365 * 2);
+              picker.$emit('pick', [start, end]);
+            }
+          }, {
+            text: '最近三年',
+            onClick(picker) {
+              const end = new Date();
+              const start = new Date();
+              start.setTime(start.getTime() - 3600 * 1000 * 24 * 365 * 3);
+              picker.$emit('pick', [start, end]);
+            }
+          }]
+        },
       option: {
         backgroundColor: '#394056',
         title: {
           top: 20,
-          text: '北向资金策略买卖点位',
+          text: '北向资金买卖策略回测',
           textStyle: {
             fontWeight: 'normal',
             fontSize: 16,
@@ -87,7 +120,7 @@ export default {
                 type:'slider',//slider表示有滑动块的，inside表示内置的
                 show:true,
                 xAxisIndex:[0],
-                start:90,
+                start:0,
                 end:100,
                 textStyle: {
                     color: '#FFFFFF'
@@ -100,7 +133,7 @@ export default {
           itemWidth: 14,
           itemHeight: 5,
           itemGap: 13,
-          data: ['当日成交净买额'],
+          data: ['目标净值', '策略净值', '持仓区间'],
           right: '4%',
           textStyle: {
             fontSize: 12,
@@ -126,7 +159,7 @@ export default {
         }],
         yAxis: [{
           type: 'value',
-          name: '净买额(亿)',
+          name: '净值',
           axisTick: {
             show: false
           },
@@ -146,16 +179,41 @@ export default {
               color: '#57617B'
             }
           }
+        },{
+          type: 'value',
+          name: '持仓区间',
+          axisTick: {
+            show: false
+          },
+          position : 'right',
+          axisLine: {
+            lineStyle: {
+              color: '#57617B'
+            }
+          },
+          axisLabel: {
+            show: false,
+            margin: 10,
+            textStyle: {
+              fontSize: 14
+            }
+          },
+          splitLine: {
+            show: true,
+            lineStyle: {
+              color: '#57617B'
+            }
+          }
         }],
         series: [{
-          name: '上限值',
+          name: '目标净值',
           type: 'line',
           smooth: true,
           symbol: 'circle',
           symbolSize: 5,
           showSymbol: false,
           tooltip: {
-              show: false
+              show: true
           },
           lineStyle: {
             normal: {
@@ -185,7 +243,7 @@ export default {
           },
           data: []
         }, {
-          name: '当日成交净买额',
+          name: '策略净值',
           type: 'line',
           smooth: true,
           symbol: 'circle',
@@ -217,15 +275,9 @@ export default {
 
             }
           },
-          data: [],
-          markPoint: {
-                symbol: 'circle',
-                symbolSize: 5,
-                showSymbol: true,
-                data:[]
-            }
+          data: []
         }, {
-          name: '下限值',
+          name: '持仓区间',
           type: 'line',
           smooth: true,
           symbol: 'circle',
@@ -234,6 +286,7 @@ export default {
           tooltip: {
               show: false
           },
+          yAxisIndex : 1,
           lineStyle: {
             normal: {
               width: 1
@@ -265,63 +318,58 @@ export default {
     };
   },
   mounted() {
-    this.queryData();
+    const start = new Date() 
+    start.setTime(start.getTime() - 3600 * 1000 * 24 * 365);
+    this.query.date = [util.formatDate(start), util.formatDate(new Date())]
   },
   methods: {
     queryData() {
-      getN2sSignTest(this.query).then((res) => {
+      if(this.query.code == null || this.query.code == ''){
+        this.$message({
+          message: '请输入代码',
+          type: 'error',
+          duration: 2 * 1000
+        })
+        return
+      }
+      if(this.query.date == null || this.query.date == ''){
+        this.$message({
+          message: '请选择时间范围',
+          type: 'error',
+          duration: 2 * 1000
+        })
+        return
+      }
+      var param = {'code': this.query.code, 
+                    'codeType': this.query.codeType,
+                    'multiple': this.query.multiple,
+                    'period': this.query.period,
+                   'startDate': this.query.date[0], 
+                   'endDate': this.query.date[1]}
+      getN2sSignTest(param).then((res) => {
         this.data = res.data;
       });
     },
+    
   },
   watch: {
       data(val) {
+          this.summary = val.summary
+          var detail = val.detail
           var date = []
-          var value = []
-          var up = []
-          var down = []
-          var markPoint = []
-          val.map(item => {
+          var net = []
+          var s_net = []
+          var point = []
+          detail.map(item => {
               date.push(item['日期'])
-              value.push(item['当日成交净买额'])
-              up.push(item['up'])
-              down.push(item['down'])
-              if(item['buy'] != null){
-                  markPoint.push({name: '买', value: '卖', xAxis: item['日期'], yAxis: item['当日成交净买额'],
-                  itemStyle: {
-                      normal: {
-                          color: '#96CF16',
-                          borderColor: '#96CF16',
-                          borderWidth: 1,
-                          label: {
-                              show: true,
-                              position: 'top'
-                          }
-                      }
-                  }
-                  })
-              }
-              if(item['sell'] != null){
-                  markPoint.push({name: '卖', value: '卖', xAxis: item['日期'], yAxis: item['当日成交净买额'], 
-                  itemStyle: {
-                      normal: {
-                          color: '#F03738',
-                          borderColor: '#F03738',
-                          borderWidth: 1,
-                          label: {
-                              show: true,
-                              position: 'buttom'
-                          }
-                      }
-                  }
-                  })
-              }
+              net.push(item['net'])
+              s_net.push(item['s_net'])
+              point.push(item['point'])
           })
           this.option.xAxis[0].data = date
-          this.option.series[0].data = up
-          this.option.series[1].data = value
-          this.option.series[1].markPoint.data = markPoint
-          this.option.series[2].data = down
+          this.option.series[0].data = net
+          this.option.series[1].data = s_net
+          this.option.series[2].data = point
       }
   }
 };
